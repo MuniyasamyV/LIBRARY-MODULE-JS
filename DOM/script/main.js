9b5db6d4b131b73aa29b12015e4a14dd7f26bdb0
giftson <fgif678@gmail.com>
2022-07-23 14:03:09 +0530

    modified styles

--- DOM/script/main.js
+++ DOM/script/main.js
@@ -1,40 +1,37 @@
 var main = createDomObj.create({
-    type:"main",
-    attr:[
-        {
-            class:"main container"
-        }
-    ]
-});
-var head = createDomObj.create(
+  type: "main",
+  attr: [
     {
-        type:"header",
-        attr:[
-            {
-                class:"d-flex flex-column"
-            }
-        ]
-    }
-);
+      class: "main container",
+    },
+  ],
+});
+var head = createDomObj.create({
+  type: "header",
+  attr: [
+    {
+      class: "d-flex flex-column",
+    },
+  ],
+});
 
 var nav = createDomObj.create({
-    type: "nav",
-  
-    attr: [
-      {
-        type: "div",
-        attr: [{ class: "flex justify-between mt-8 mb-4 pb-4 border-b" }],
-        child: [
-          {
-            type: "div",
-            text: "Subscribe",
-            
-          },
-        ],
-      },
-    ],
-  });
-  head.addChild(nav);
-  main.addChild(head);
-  var domm = createDomObj(main);
-  document.body.append(domm);
+  type: "nav",
+
+  attr: [
+    {
+      type: "div",
+      attr: [{ class: "flex justify-between mt-8 mb-4 pb-4 border-b" }],
+      child: [
+        {
+          type: "div",
+          text: "Subscribe",
+        },
+      ],
+    },
+  ],
+});
+head.addChild(nav);
+main.addChild(head);
+var domm = createDomObj(main);
+document.body.append(domm);
